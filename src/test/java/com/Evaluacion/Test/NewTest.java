db9027453e77854e070b5b965a665d03df704eef
unknown <nco.ralf@gmail.com>
2022-01-10 13:36:56 -0300

    first commit

---
+++ src/test/java/com/Evaluacion/Test/NewTest.java
@@ -0,0 +1,43 @@
+package com.Evaluacion.Test;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
+
+
+
+
+@SuppressWarnings("unused")
+
+public class NewTest {
+	public NewTest() {
+		
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+		
+	}
+	@AfterClass
+	public static void tearDownClass() {
+		
+	}
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@After
+	public void tearDown(){
+		
+	}
+	
+  @Test
+  public void f() {
+  }
+}
